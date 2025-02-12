f15143bdf56cdbf9003cd5254e642f9b2f2c7159
hb9397 <hb9397@naver.com>
2025-02-12 17:56:24 +0900

    init :

---
+++ src/main/java/egovframework/com/config/EgovConfigApp.java
@@ -0,0 +1,26 @@
+package egovframework.com.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@Import({
+	EgovConfigAppAspect.class,
+	EgovConfigAppCommon.class,
+	EgovConfigAppDatasource.class,
+	EgovConfigAppIdGen.class,
+	EgovConfigAppProperties.class,
+	EgovConfigAppMapper.class,
+	EgovConfigAppTransaction.class,
+	EgovConfigAppValidator.class,
+	EgovConfigAppWhitelist.class
+})
+@PropertySources({
+	@PropertySource("classpath:/egovframework/egovProps/globals.properties")
+	//@PropertySource("classpath:/application.yaml")
+}) //CAUTION: min JDK 8
+public class EgovConfigApp {
+
+}
